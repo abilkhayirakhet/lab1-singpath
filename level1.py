707b5def20b0fc69cea99cff7917c61c887a7104
akhet <akhet@kolesa.kz>
2018-03-17 21:16:36 +0600

    inception of labwork 1

---
+++ level1.py
@@ -0,0 +1,14 @@
+# SingPath's level_1 task
+# Implementer: Abilkhayir Akhet
+
+# task 1
+print('1 task\'s label: "Welcome task"')
+print("Result of task 1: Here I don't have to type any code")
+
+# task 2
+print('2 task\'s label: "Your First Program"')
+greeting = 'Hello World'
+print("Result of task 2: "+greeting)
+
+# task 3
+print('3 task\'s label: "Your First Program"')
